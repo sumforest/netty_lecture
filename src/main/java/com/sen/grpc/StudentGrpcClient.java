629847f904cc18111f11dd8d32f97d5171f2c38d
sumforest <1483026405@qq.com>
2020-07-13 23:10:08 +0800

    GRPC 流式数据交互

--- src/main/java/com/sen/grpc/StudentGrpcClient.java
+++ src/main/java/com/sen/grpc/StudentGrpcClient.java
@@ -2,13 +2,19 @@ package com.sen.grpc;
 
 import com.sen.proto.MyRequest;
 import com.sen.proto.MyResponse;
+import com.sen.proto.StreamRequest;
+import com.sen.proto.StreamResponse;
+import com.sen.proto.StudentList;
 import com.sen.proto.StudentRequest;
 import com.sen.proto.StudentResponse;
 import com.sen.proto.StudentServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
+import java.time.LocalDateTime;
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author sen
@@ -17,7 +23,7 @@ import java.util.Iterator;
  */
 public class StudentGrpcClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8899)
                 .usePlaintext()
                 .build();
@@ -32,5 +38,73 @@ public class StudentGrpcClient {
             StudentResponse streamResponse = streamIterator.next();
             System.out.println(streamResponse.getUsername() + ", " + streamResponse.getAge() + ", " + streamResponse.getCity());
         }
+
+        System.out.println("----------- 客户端发送一个流式对象，服务端响应一个对象 -----------");
+        // 构建一个异步的桩
+        StudentServiceGrpc.StudentServiceStub studentServiceStub = StudentServiceGrpc.newStub(channel);
+        // 构建一个发送流式数据后回调处理服务端数据的Observer
+        StreamObserver<StudentList> studentListStreamObserver = new StreamObserver<StudentList>() {
+            /**
+             *  接受到服务端数据后被回调
+             * @param value
+             */
+            @Override
+            public void onNext(StudentList value) {
+                System.out.println("客户端发送一个流式对象，接收到服务端响应：");
+                value.getListList().forEach(studentResponse -> {
+                    System.out.println(studentResponse.getUsername());
+                    System.out.println(studentResponse.getAge());
+                    System.out.println(studentResponse.getCity());
+                    System.out.println("***********************");
+                });
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("completed");
+            }
+        };
+        StreamObserver<StudentRequest> streamObserver = studentServiceStub.getUsersByAges(studentListStreamObserver);
+        streamObserver.onNext(StudentRequest.newBuilder().setAge(20).build());
+        streamObserver.onNext(StudentRequest.newBuilder().setAge(30).build());
+        streamObserver.onNext(StudentRequest.newBuilder().setAge(40).build());
+        streamObserver.onCompleted();
+        // 因为异步所以不会等待客户端返回所以需要阻塞主线程
+        TimeUnit.SECONDS.sleep(5);
+
+        System.out.println("----- 客户端发送一个流式数据，服务端返回一个流式数据 ----");
+        StreamObserver<StreamResponse> streamObserverResponse = new StreamObserver<StreamResponse>() {
+            /**
+             * 接收到服务器响应回调
+             * @param value
+             */
+            @Override
+            public void onNext(StreamResponse value) {
+                System.out.println("接收到服务器响应" + value.getResponseInfo());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("completed");
+            }
+        };
+        StreamObserver<StreamRequest> streamRequestStreamObserver = studentServiceStub.biTack(
+                streamObserverResponse);
+        for (int i = 0; i < 10; i++) {
+            streamRequestStreamObserver.onNext(StreamRequest.newBuilder().setRequestInfo(LocalDateTime.now().toString()).build());
+        }
+        streamRequestStreamObserver.onCompleted();
+        // 因为异步所以不会等待客户端返回所以需要阻塞主线程
+        TimeUnit.SECONDS.sleep(5);
     }
 }
--- src/main/java/com/sen/grpc/StudentServiceImpl.java
+++ src/main/java/com/sen/grpc/StudentServiceImpl.java
@@ -2,10 +2,15 @@ package com.sen.grpc;
 
 import com.sen.proto.MyRequest;
 import com.sen.proto.MyResponse;
+import com.sen.proto.StreamRequest;
+import com.sen.proto.StreamResponse;
+import com.sen.proto.StudentList;
 import com.sen.proto.StudentRequest;
 import com.sen.proto.StudentResponse;
 import com.sen.proto.StudentServiceGrpc;
 import io.grpc.stub.StreamObserver;
+
+import java.util.UUID;
 
 /**
  * @author sen
@@ -14,6 +19,12 @@ import io.grpc.stub.StreamObserver;
  */
 public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBase {
 
+    /**
+     * GRPC数据格式1：客户端发送一个参数，服务端返回一个参数
+     *
+     * @param request
+     * @param responseObserver
+     */
     @Override
     public void getRealNameByUsername(MyRequest request, StreamObserver<MyResponse> responseObserver) {
         System.out.printf("服务段被调用,收到参数：%s\n", request.getUsername());
@@ -59,5 +70,77 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
         );
         // 通知stream操作结束
         responseObserver.onCompleted();
+    }
+
+    /**
+     * 客户端发送一个流式数据，服务端返回一个对象
+     *
+     * @param responseObserver
+     * @return
+     */
+    @Override
+    public StreamObserver<StudentRequest> getUsersByAges(StreamObserver<StudentList> responseObserver) {
+        return new StreamObserver<StudentRequest>() {
+            /**
+             * 服务端接收到客户端的数据时被回调
+             * @param value
+             */
+            @Override
+            public void onNext(StudentRequest value) {
+                System.out.println("服务端接收到客户端的流式数据：" + value);
+            }
+
+            /**
+             * 发生异常时被回调
+             * @param t
+             */
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+
+            /**
+             * 客户端链接完成流式数据传输时被回调，用于响应一个对象给客户端
+             */
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(
+                        StudentList.newBuilder()
+                                .addList(StudentResponse.newBuilder().setUsername("张三").setAge(22).setCity("深圳").build())
+                                .addList(StudentResponse.newBuilder().setUsername("李四").setAge(24).setCity("深圳").build())
+                                .build()
+                );
+                // 结束本次操作
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    /**
+     * 客户端发送一个流式数据，服务端返回一个流式数据
+     * @param responseObserver
+     * @return
+     */
+    @Override
+    public StreamObserver<StreamRequest> biTack(StreamObserver<StreamResponse> responseObserver) {
+        return new StreamObserver<StreamRequest>() {
+            @Override
+            public void onNext(StreamRequest value) {
+                System.out.println("客户端发送一个流式数据，服务端返回一个流式数据;接收客户端参数: "+value.getRequestInfo());
+                // 接收到参数的同时响应数据给服务端
+                responseObserver.onNext(StreamResponse.newBuilder().setResponseInfo(UUID.randomUUID().toString()).build());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                // 当客户端完成时，服务端也完成
+                responseObserver.onCompleted();
+            }
+        };
     }
 }
